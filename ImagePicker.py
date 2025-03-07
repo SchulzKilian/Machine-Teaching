8469d47fa9d4e1db47c5a21553515c1afc7ee9fd
SchulzKilian <schulz.kilian@outlook.de>
2025-03-07 18:40:49 +0100

    starting to implement better dynamic picking

---
+++ ImagePicker.py
@@ -0,0 +1,14 @@
+
+
+
+class ImagePicker:
+    def __init__(self, dataset):
+        self.dataset = dataset
+    
+
+    def get_images(self, amount):
+        return self.dataset
+    
+
+    def order(self):
+        pass
