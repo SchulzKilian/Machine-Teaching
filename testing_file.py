6a21022a78297fed0c81ca3c4e1ac38c9e03ad14
SchulzKilian <schulz.kilian@outlook.de>
2025-03-08 16:47:41 +0100

    cats vs dogs implemented properly

--- testing_file.py
+++ testing_file.py
@@ -17,11 +17,12 @@ doglabels = [34, 21, 6, 33, 8, 1, 24, 27, 12, 10, 28, 7]
 batch_size = 4
 learning_rate = 0.1
 num_epochs = 10
-data_size = 800
-arg = "pets"
+data_size = 1600
+arg = "catsvdogs"
 
 class CustomImageDataset(Dataset):
-    def __init__(self, folder_path, num_samples):
+    def __init__(self, folder_path, num_samples, transform):
+        self.transform = transform
         self.images = []
         self.labels = []
         
@@ -44,14 +45,18 @@ class CustomImageDataset(Dataset):
         return len(self.images)
 
     def __getitem__(self, idx):
-        image_path = self.images[idx]
+
+        image_raw = self.images[idx]
+        image = Image.open(image_raw)
+        if self.transform:
+            image = self.transform(image)
         label = self.labels[idx]
-        image = Image.open(image_path).convert('RGB')  # Open image and convert to RGB
         return image, label
 
 
 class SubsetDataset(Dataset):
     def __init__(self, dataset, num_samples):
+        self.transform = transform
         self.dataset = dataset
         self.num_samples = num_samples
 
@@ -60,6 +65,8 @@ class SubsetDataset(Dataset):
         return self.num_samples
 
     def __getitem__(self, idx):
+
+
         if arg !=  "catsvdogs":
             return self.dataset[idx]
         image, label = self.dataset[idx]
@@ -105,17 +112,24 @@ elif arg == "pets":
 elif arg == "catsvdogs":
     folder_path = './pets/oxford-iiit-pet/images'
     channels = 3
-    classes = 37
+    classes = 2
     transform = transforms.Compose([
     transforms.Resize((224, 224)),  # Resize the images to match the input size of ResNet50
     transforms.ToTensor(),          # Convert the images to PyTorch tensors
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize the images
     
 ])
+    full_dataset = CustomImageDataset(folder_path, num_samples=data_size, transform=transform)
+    
+    # Split into train/test (80-20 split)
+    train_size = int(0.8 * len(full_dataset))
+    test_size = len(full_dataset) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
 
 
-train_dataset = SubsetDataset(train_dataset, data_size)
-test_dataset = SubsetDataset(test_dataset,data_size)
+if arg != "catsvdogs":
+    train_dataset = SubsetDataset(train_dataset, data_size)
+    test_dataset = SubsetDataset(test_dataset,data_size)
 
 
 
@@ -126,6 +140,7 @@ train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 def decide_callback(epoch, number):
+    return False
     if number in [1,2] and epoch % 3 == 0:
         return True
     else:
